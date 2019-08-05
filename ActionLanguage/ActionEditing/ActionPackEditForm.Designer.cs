b87ab229e74b76c2f46826f232dab350a3cf222b
robbyxp1 <robxp@hotmail.com>
2019-08-05 15:48:04 +0100

    Remove need for internalscrollbar from panelscroll

--- ActionLanguage/ActionEditing/ActionPackEditForm.Designer.cs
+++ ActionLanguage/ActionEditing/ActionPackEditForm.Designer.cs
@@ -228,8 +228,6 @@
             this.panelVScroll.Controls.Add(this.vScrollBarCustom1);
             this.panelVScroll.Controls.Add(this.buttonMore);
             this.panelVScroll.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelVScroll.InternalScrollbar = true;
-            this.panelVScroll.Location = new System.Drawing.Point(3, 3);
             this.panelVScroll.Name = "panelVScroll";
             this.panelVScroll.Size = new System.Drawing.Size(1060, 502);
             this.panelVScroll.TabIndex = 8;
